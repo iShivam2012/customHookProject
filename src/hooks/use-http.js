70ad5cfb216abb987f6223bf5e5cf29621669e1e
iShivam2012 <shivam.gupta97a@gmail.com>
2021-06-25 00:49:56 +0530

    implementing the useHttp customHook

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,7 +1,39 @@
 import {useState, useEffect} from 'react';
 
-const useHttp = () => {
+const useHttp = (requestObj, onData) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [tasks, setTasks] = useState([]);
+  
+    const sendRequest = async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          requestObj.url, {
+              method: requestObj.method,
+              headers: requestObj.headers,
+              body: JSON.stringify(requestObj.body)
+          }
+        );
+  
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+  
+        const data = await response.json();
+        onData(data);
 
+        } catch (err) {
+        setError(err.message || "Something went wrong!");
+      }
+      setIsLoading(false);
+    };
+    return {
+        isLoading,
+        error,
+        sendRequest
+    }
 }
 
 export default useHttp;
